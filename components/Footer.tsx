b375bd3969e40f2dc81ede87dd67e0e32472e631
João Batista <joaocardoso_miranda@outlook.com>
2024-07-08 20:08:11 -0300

    alterations

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
         </a>
       </div>
 
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex mt-16 md:mb-5 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 João Batista
         </p>
