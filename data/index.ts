7ce4a21a164385bf5d71f5a638c71fe8dbbd180a
João Batista <126399807+Cardosofiles@users.noreply.github.com>
2024-11-22 23:22:12 -0300

    changing resume and linkedin profile url

--- data/index.ts
+++ data/index.ts
@@ -230,7 +230,7 @@ export const socialMedia = [
   {
     id: 1,
     img: "/link.svg",
-    link: "https://www.linkedin.com/in/jo%C3%A3o-batista-2b0442268/",
+    link: "www.linkedin.com/in/joão-batista-dev-front-end",
   },
   {
     id: 2,
